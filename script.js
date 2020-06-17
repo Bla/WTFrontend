25a8136eab21924c302a06f67f5c4c421ec48836
Bla <21099480+Bla@users.noreply.github.com>
2020-06-17 11:41:39 +0200

    add hover effects

--- script.js
+++ script.js
@@ -2,6 +2,28 @@ const SPELER1_KLEUR = "red"
 
 function vakjeVullen(vakjeId) {
     var elem = document.getElementById(vakjeId);
-    elem.bgColor = SPELER1_KLEUR;
-    elem.style.border = "solid red";
+    var gespeeld = elem.getAttribute("data-gespeeld");
+    if (gespeeld == "none") {
+        elem.bgColor = SPELER1_KLEUR;
+        elem.style.border = "solid red";
+        elem.setAttribute("data-gespeeld", "speler1");
+    }
+}
+
+function tijdelijkVullen(vakjeId) {
+    var elem = document.getElementById(vakjeId);
+    var gespeeld = elem.getAttribute("data-gespeeld");
+    if (gespeeld == "none") {
+        elem.bgColor = SPELER1_KLEUR;
+        elem.style.border = SPELER1_KLEUR;
+    }
+}
+
+function vakjeReset(vakjeId) {
+    var elem = document.getElementById(vakjeId);
+    var gespeeld = elem.getAttribute("data-gespeeld");
+    if (gespeeld == "none") {
+    elem.bgColor = "white";
+    elem.style.border = "solid black";
+    }
 }
