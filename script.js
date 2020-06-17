d3aeb8f9f5c4d388a32e781a3c91b93629599edc
Bla <21099480+Bla@users.noreply.github.com>
2020-06-17 12:57:24 +0200

    create table dynamically

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
+const TABEL_BREEDTE = 7
+const TABEL_HOOGTE = 6
 const SPELER1_KLEUR = "red"
+
+function maakTabel(tabelId) {
+    var tabel = document.getElementById("gameTabel");
+    console.log(tabel);
+    for (var y=0; y<TABEL_HOOGTE; y++) {
+        var rij = document.createElement("tr");
+        tabel.appendChild(rij);
+        for (var x=0; x<TABEL_BREEDTE; x++) {
+            var id = "vakje" + x + y;
+            var kolom = document.createElement("td");
+            kolom.setAttribute("id", id);
+            kolom.setAttribute("data-gespeeld", "none");
+            kolom.setAttribute("onmouseover", "tijdelijkVullen(this.id)");
+            kolom.setAttribute("onmouseout", "vakjeReset(this.id)");
+            kolom.setAttribute("onclick","vakjeVullen(this.id)");
+            rij.appendChild(kolom);
+        }
+    }
+}
 
 function vakjeVullen(vakjeId) {
     var elem = document.getElementById(vakjeId);
